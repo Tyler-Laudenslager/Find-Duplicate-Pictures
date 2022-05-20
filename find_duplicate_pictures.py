d66cb6ea2d8400198162514da679de05606eb0a3
tyl.laud <61608238+Tyler-Laudenslager@users.noreply.github.com>
2022-05-20 10:05:48 -0400

    Update find_duplicate_pictures.py

--- find_duplicate_pictures.py
+++ find_duplicate_pictures.py
@@ -4,12 +4,12 @@
 #Date: 5/19/2022
 #Purpose: Detect Duplicate Pictures Across A Directory - Recursive Search
 #
-#Programmed On Computer Using Solar Energy - Green Code
+
 
 import os
 import sys
 
-def find_duplicates(path):
+def find_duplicates(path) -> dict():
     all_files = dict()
     for item in os.listdir(path):
         if os.path.isdir(path + "/" + item):
@@ -19,12 +19,13 @@ def find_duplicates(path):
                 else:
                     all_files[file_name] = [*locations]
         else:
+            item = item.lower()
             #https://stackoverflow.com/questions/889333/how-to-check-if-a-file-is-a-valid-image-file#902779
-            if item.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
-                if item.lower() in all_files:
-                    all_files[item.lower()].append(path)
+            if item.endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
+                if item in all_files:
+                    all_files[item].append(path)
                 else:
-                    all_files[item.lower()] = [path]
+                    all_files[item] = [path]
             else:
                 continue
     return all_files
@@ -35,13 +36,13 @@ if __name__ == "__main__":
         print(f"Usage: {sys.argv[0]} path_to_directory -> example C:/Pictures")
     else:
         print()
-        print("Duplicate Files Found")
-        for duplicate_file, locations in find_duplicates(sys.argv[1]).items():
+        duplicates_found = find_duplicates(sys.argv[1]).items()
+        print(f"Duplicate Files Found -> {len(duplicates_found)} Files")
+        for duplicate_file, locations in duplicates_found:
             if len(locations) > 1:
-                print("="*20)
+                print()
                 print(f"Duplicate File: {duplicate_file}")
-                print("-"*20)
-                print("Found in Locations Listed Below")
+                print("-"*40)
+                print("Locations Found:")
                 print(*['\t'+x for x in locations], sep="\n")
-                print("="*20)
 
